d6520fbd29f1059ac3a61c6132e9e88a428b11dc
Пользователь <world.worensi@gmail.com>
2023-12-06 13:39:06 +0600

    add pagination css

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -12,16 +12,16 @@ const Pagination = ({onClick}) => {
 
     return (
 
-        <div>
-            <button onClick={()=>handleChange(active-1)} disabled={active===1}>Назад</button>
+        <div className={'pagination-container'}>
+            <button className={'innerBtn'} onClick={()=>handleChange(active-1)} disabled={active===1}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path></svg></button>
             {
                 newArr.map((item, index) =>
-                    <button onClick={() => handleChange(index + 1)} key={index} style={{background:active===index+1?'green':'white'}}>
+                    <button className={'btn-pagination'} onClick={() => handleChange(index + 1)} key={index} style={{background:active===index+1?'linear-gradient(180deg, #6c1f41 0%, #210d0e 100%)':'transparent'}}>
                         {index + 1}
                     </button>
                 )
             }
-            <button onClick={()=>handleChange(active+1)} disabled={active===10}>Вперед</button>
+            <button className={'innerBtn'} onClick={()=>handleChange(active+1)} disabled={active===10}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path></svg></button>
         </div>
     )
 
